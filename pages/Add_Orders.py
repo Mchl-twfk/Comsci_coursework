861431957fc6e216d35c2e65c4e3a11fa09f7ebb
Mchl-twfk <18tawfik@parmiters.herts.sch.uk>
2024-02-14 10:51:51 +0000

    text input added to add order page

--- pages/Add_Orders.py
+++ pages/Add_Orders.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
-st.title("-Add an Order_")
+st.title("-Add an Order-")
+
+tablenum = st.number_input("Table Number", min_value=1, max_value=50)
+
+order = st.text_input("Order")
+
+
