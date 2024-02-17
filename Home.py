83a279ed0df2ce71a5fbb728508eedfd55b8160b
Mchl-twfk <18tawfik@parmiters.herts.sch.uk>
2024-02-17 12:45:40 +0000

    connect db and add buttons

--- Home.py
+++ Home.py
@@ -1,5 +1,19 @@
 import streamlit as st
+import sqlite3
+
+
+con = sqlite3.connect("orders.db")
+cur = con.cursor()
 
 st.title("Resturant order system")
 
 st.write("-please select which page you would like to visit-")
+
+def create_table():
+    cur.execute("""
+                CREATE TABLE IF NOT EXISTS orders(
+                table number INTEGER,
+                order TEXT,
+                ready? BOOLEAN
+                )
+                """)
--- pages/Veiw_Orders.py
+++ pages/Veiw_Orders.py
@@ -1,3 +1,19 @@
 import streamlit as st
+import sqlite3
+from Home import create_table
+
+con = sqlite3.connect("orders.db")
+cur = con.cursor()
+
 
 st.title("-Orders-")
+
+clear_button = st.button("clear table", type="primary")
+
+if clear_button:
+    st.write("are you sure?")
+    yes_button = st.button("Yes")
+    no_button = st.button("No")
+
+st.table(create_table)
+
