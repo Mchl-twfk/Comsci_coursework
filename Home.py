ff5d4c81dd48643cc20f8ae020bf51910786efa2
Mchl-twfk <18tawfik@parmiters.herts.sch.uk>
2024-03-01 23:02:05 +0000

    first commit

--- Home.py
+++ Home.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import sqlite3
 import pandas as pd
+from contextlib import closing  
+
 
 ##link sql database to python program
 con = sqlite3.connect("orders.db")
@@ -10,13 +12,25 @@ cur = con.cursor()
 st.title("Restaurant order system")
 st.write("-please select which page you would like to visit-")
 
+##SQL Query runner
+def Query(SQL, inputs=()):
+    with closing(sqlite3.connect("orders.db")) as con:
+        with con:
+            with closing(con.cursor()) as cur:
+                if inputs == ():
+                    Records = cur.execute(SQL)
+                    return Records.fetchall()
+                else:
+                    cur.execute(SQL, inputs)
+
 ##create table in the database
 def create_table():
 
-    cur.execute("""
+    Query("""
                 CREATE TABLE IF NOT EXISTS orders(
                 table number INTEGER,
                 order TEXT,
+                in progress BOOLEAN,
                 ready? BOOLEAN
                 )
                 """)
@@ -26,5 +40,7 @@ def add_order():
     cur.execute(f"""
                 INSERT INTO orders
                 st.session_state.orderlist""")
-    
-##
+
+
+
+
